3b0cbcdf0ee7ced2b5e8822d0bd20a080b3256ba
Kevin Bai <lkevin957@gmail.com>
2019-03-11 16:05:30 -0700

    Refactor index to a jsx file

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -4,10 +4,11 @@ import Root from './components/Root.jsx';
 import configureStore from './store/store';
 import jwt_decode from 'jwt-decode';
 import { setAuthToken } from './util/sessionApiUtil';
-import { logout } from './actions/sessionActions'
+import { logout } from './actions/sessionActions';
 
 document.addEventListener('DOMContentLoaded', () => {
     let store;
+    const root = document.getElementById('root');
 
     if (localStorage.jwtToken) {
         setAuthToken(localStorage.jwtToken);
@@ -27,6 +28,9 @@ document.addEventListener('DOMContentLoaded', () => {
         store = configureStore({});
     }
 
-    ReactDOM.render(<Root store={store} />, document.getElementById('root'));
-})
+    ReactDOM.render(
+        <Root store={store} />,
+        root
+    );
+});
 
