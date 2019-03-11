6d1b91b7b5926d03ec573fd758d650eb80f664a5
Kevin Bai <lkevin957@gmail.com>
2019-03-11 14:14:30 -0700

    Add books reducer

--- frontend/src/reducers/rootReducer.js
+++ frontend/src/reducers/rootReducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import session from './sessionReducer';
+import books from './booksReducer';
 
 const RootReducer = combineReducers({
-    session
+    session,
+    books
 });
 
 export default RootReducer;
