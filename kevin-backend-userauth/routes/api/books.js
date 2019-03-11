3be5c9d63e86033e988a3a085947a3cf672df791
Kevin Bai <lkevin957@gmail.com>
2019-03-11 11:46:46 -0700

    Run createReactApp, configure proxy to be localhost 5000

--- kevin-backend-userauth/routes/api/books.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.get("/test", (req, res) => res.json({
-    msg: "this is the books route"
-}));
-
-module.exports = router;
