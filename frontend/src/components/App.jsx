07a91d1657e78201eec49cbcccfbe037a2d817fa
Kevin Bai <lkevin957@gmail.com>
2019-03-11 15:43:55 -0700

    Add coments to mark where things should go in App

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -1,6 +1,19 @@
 import React, { Component } from 'react';
-import LoginFormContainer from './session/loginFormContainer';
 import { Switch, Route } from 'react-router-dom';
+
+// App
+  // navBarContainer
+  // MainPage
+
+// Auth Related
+import LoginFormContainer from './session/loginFormContainer';
+  // SignupFormContainer
+
+// Book Related
+  // bookIndex
+
+// Search Related
+  // searchBar
 
 class App extends Component {
   render() {
