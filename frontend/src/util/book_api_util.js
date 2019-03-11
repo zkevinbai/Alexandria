d5153d654e0f459300c01a8c8ce40c307ba523f4
Kevin Bai <lkevin957@gmail.com>
2019-03-11 13:55:23 -0700

    Add corresponding action and thunk action for query

--- frontend/src/util/book_api_util.js
+++ frontend/src/util/book_api_util.js
@@ -1,5 +1,9 @@
 import axios from 'axios';
 
+export const queryBooks = (queryString) => {
+    return axios.get(`https://www.googleapis.com/books/v1/volumes?q={${queryString}}`);
+};
+
 export const getBooks = (userId) => {
     return axios.get(`api/books/user/${userId}`);
 };
@@ -11,3 +15,4 @@ export const addBook = (data) => {
 export const deleteBook = (data) => {
     return axios.post(`api/books/user/${data.userId}`, data.bookId);
 };
+
