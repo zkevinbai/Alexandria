5cbae837ce4c17f08002f0231fb15326097cd239
Sam <sam.d.odonnell@gmail.com>
2019-03-11 20:04:40 -0700

    Add splash page and beginning css

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -5,6 +5,7 @@ import SignupFormContainer from './session/signupFormContainer';
 import { Switch, Route } from 'react-router-dom';
 import LogoutButtonContainer from './session/logoutButtonContainer';
 import { AuthRoute, ProtectedRoute } from '../util/routeUtil';
+import Splash from './Splash';
 
 // App
   // navBarContainer
@@ -24,6 +25,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <Route path="/" component={Splash} />
         <ProtectedRoute path="/" component={LogoutButtonContainer} />
         <Switch>
           <Route exact path="/" render={() => <h1>It worked!</h1>} />
---
+++ frontend/src/components/Splash.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Splash() {
+  return (
+    <div className="splash-wrapper">
+      <h1>Alexandria</h1>
+      <h2>A place for books</h2>
+    </div>
+  )
+}
