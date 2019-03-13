3e3fc00532e056c3a96dcf90fb6c8e34f0338cd1
Kevin Bai <lkevin957@gmail.com>
2019-03-13 11:42:57 -0700

    Refactor searchbar dropdown to still add books for users until that show is built

--- frontend/src/components/search/dropdown/searchBarDropdownItem.jsx
+++ frontend/src/components/search/dropdown/searchBarDropdownItem.jsx
@@ -16,7 +16,9 @@ export default class searchBarDropdownItem extends Component {
             this.props.addUserBook({
                 userId: this.props.userId,
                 book
-            });
+            }).then(
+                this.props.clearSearch()
+            )
         };
     }
 
@@ -75,30 +77,32 @@ export default class searchBarDropdownItem extends Component {
         if(this.props.modalType === "public"){
             return this.publicShow()
         } else if (this.props.modalType === "userNew"){
-            return this.userNew()
+            return(
+                <div
+                    className="query-dropdown-index-item"
+                    onClick={this.handleClick(this.props.book)}
+                >
+                    <img src={this.props.book.imageUrl} />
+                    <div
+                        className="query-dropdown-index-item-text"
+                    >
+                        <h1
+                            className="query-dropdown-index-item-title"
+                        >
+                            {this.props.book.title.slice(0, 50)}
+                        </h1>
+
+                        <h1
+                            className="query-dropdown-index-item-author"
+                        >
+                            {this.props.book.author}
+                        </h1>
+                    </div>
+                </div>
+            )
         }
     }
 }
 
+// return this.userNew() add this in when userNew is complete
 
-// <div
-//     className="query-dropdown-index-item"
-//     onClick={this.handleClick(this.props.book)}
-// >
-//     <img src={this.props.book.imageUrl} />
-//     <div
-//         className="query-dropdown-index-item-text"
-//     >
-//         <h1
-//             className="query-dropdown-index-item-title"
-//         >
-//             {this.props.book.title.slice(0, 50)}
-//         </h1>
-
-//         <h1
-//             className="query-dropdown-index-item-author"
-//         >
-//             {this.props.book.author}
-//         </h1>
-//     </div>
-// </div>
