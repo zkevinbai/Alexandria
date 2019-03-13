7a50c20b3072b8d981f7563a32dedd994122c30d
Kevin Bai <lkevin957@gmail.com>
2019-03-12 21:08:14 -0700

    Add book public show container with no action

---
+++ frontend/src/components/books/bookshow/bookPublicShowContainer.js
@@ -0,0 +1,12 @@
+import { connect } from 'redux';
+import BookShow from './bookShow';
+
+const mapStateToProps = (storeState) = {
+
+};
+
+const mapDispatchToProps = (dispatch) = ({
+    
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(BookShow);
