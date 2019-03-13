8cc38b7578d3b1ebf2f8f79f4c0a98e37805c730
Kevin Bai <lkevin957@gmail.com>
2019-03-13 11:49:40 -0700

    Add basic styling to book show modal to set positioning, background and general appearance

--- frontend/src/components/books/bookshow/bookShow.jsx
+++ frontend/src/components/books/bookshow/bookShow.jsx
@@ -13,27 +13,29 @@ export default class bookShow extends Component {
     render() {
         return (
         <div className="book-show">
-            <img src={this.props.book.imageUrl} />
-            <div
-                className="book-show-heading-text"
-            >
-                <h1>{this.props.book.title}</h1>
-                <h1>{this.props.book.author}</h1>
-            </div>
+            <div className="book-show-content">
+                <img src={this.props.book.imageUrl} />
+                <div
+                    className="book-show-heading-text"
+                >
+                    <h1>{this.props.book.title}</h1>
+                    <h1>{this.props.book.author}</h1>
+                </div>
 
-            <div
-                className="book-show-info-text"
-            >
-                <h1>Published: {this.props.book.publishedDate}</h1>
-                <h1>Genre: {this.props.book.genre}</h1>
-                <h1>Page Count: {this.props.book.pageCount}</h1>
-            </div>
+                <div
+                    className="book-show-info-text"
+                >
+                    <h1>Published: {this.props.book.publishedDate}</h1>
+                    <h1>Genre: {this.props.book.genre}</h1>
+                    <h1>Page Count: {this.props.book.pageCount}</h1>
+                </div>
 
-            <p
-                className="book-show-description-text"
-            >
-                {this.props.book.description}
-            </p>
+                <p
+                    className="book-show-description-text"
+                >
+                    {this.props.book.description}
+                </p>
+            </div>
         </div>
         )
     }
