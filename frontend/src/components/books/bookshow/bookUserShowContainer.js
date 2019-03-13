e4afe6de38166eaf7345f77c275060992b142050
Kevin Bai <lkevin957@gmail.com>
2019-03-12 21:02:30 -0700

    Add book show containers for public, usernew and user show

---
+++ frontend/src/components/books/bookshow/bookUserShowContainer.js
@@ -0,0 +1,13 @@
+import {connect} from 'redux';
+import BookShow from './bookShow';
+import { removeUserBook } from '../../../actions/bookActions';
+
+const mapStateToProps = (storeState) = {
+
+};
+
+const mapDispatchToProps = (dispatch) = ({
+    removeUserBook: (data) => dispatch(removeUserBook(data)) 
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(BookShow);
