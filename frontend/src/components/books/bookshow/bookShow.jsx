aecbf9f9d87ca7eff84d4728a89c9fd0a136202c
Kevin Bai <lkevin957@gmail.com>
2019-03-19 17:04:04 -0700

    Add book show headline container class for title and close out button

--- frontend/src/components/books/bookshow/bookShow.jsx
+++ frontend/src/components/books/bookshow/bookShow.jsx
@@ -14,10 +14,13 @@ export default class bookShow extends Component {
         return (
         <div className="book-show">
             <div className="book-show-content">
+                <div className="book-show-headline">                
+                <h1>{this.props.book.title}</h1>
                 <i 
                     className="fas fa-times"
                     onClick={() => this.props.history.goBack()}
                 ></i>
+                </div>
                 <div className="book-show-head">
                     <img src={this.props.book.imageUrl} />
                     <div
@@ -26,7 +29,6 @@ export default class bookShow extends Component {
                         <div
                             className="book-show-heading-text"
                         >
-                            <h1>{this.props.book.title}</h1>
                             <h1>{this.props.book.author}</h1>
                         </div>
 
