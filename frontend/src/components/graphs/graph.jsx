d36a1819053e9182227fd745ad76ab861433fd12
Kevin Bai <lkevin957@gmail.com>
2019-04-03 16:55:40 -0700

    Add donut to graph

--- frontend/src/components/graphs/graph.jsx
+++ frontend/src/components/graphs/graph.jsx
@@ -22,7 +22,8 @@ class Graph extends React.Component {
       .attr("transform", "translate(300, 300)"); // set center of pie to 300,300
 
     const arc = d3.arc()
-      .innerRadius(0)//makes a closed pie chart
+      // .innerRadius(0)//makes a closed pie chart
+      .innerRadius(120)//makes a donut chart
       .outerRadius(r);
 
     const pie = d3.pie()
@@ -49,8 +50,8 @@ class Graph extends React.Component {
       .attr("transform", function (d) { return "translate(" + arc.centroid(d) + ")"; }) // put text at the center of every arc
       .attr("text-anchor", "middle")
       .attr("font-size", "1em")
+      // .attr("font-family", "Source Sans Pro")
       .text( function (d, i) { return data.label[i]; });
-
   }
   
   getGenreArray() {
